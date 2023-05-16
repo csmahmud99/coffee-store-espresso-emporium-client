cdb0bdef66ceeafd095e3a64501948c5cb0dd7c6
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-16 17:58:30 +0600

    Data Fetch Done for Update Coffee Details

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
@@ -55,7 +56,7 @@ const CoffeeCard = ({ coffee }) => {
                         <div className="card-actions justify-end">
                             <div className="btn-group btn-group-vertical space-y-2">
                                 <button className="btn">View</button>
-                                <button className="btn">Edit</button>
+                                <Link to={`/update-coffee/${_id}`}><button className="btn">Edit</button></Link>
                                 <button onClick={() => handleDelete(_id)} className="btn bg-orange-500">X</button>
                             </div>
                         </div>
