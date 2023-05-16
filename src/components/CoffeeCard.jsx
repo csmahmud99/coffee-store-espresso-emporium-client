40feaa5387a4e130c4c105ff6114b15c5cc13f94
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-16 17:44:01 +0600

    Single Coffee Item Delete Option, Done

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,5 +1,42 @@
+import Swal from 'sweetalert2';
+
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, chef, supplier, taste, category, photo } = coffee;
+    const { _id, name, quantity, chef, supplier, taste, category, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire(
+                //     'Deleted!',
+                //     'Your file has been deleted.',
+                //     'success'
+                // );
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been Deleted.',
+                                'success'
+                            );
+                        }
+                    });
+            }
+        });
+    };
 
     return (
         <div>
@@ -19,7 +56,7 @@ const CoffeeCard = ({ coffee }) => {
                             <div className="btn-group btn-group-vertical space-y-2">
                                 <button className="btn">View</button>
                                 <button className="btn">Edit</button>
-                                <button className="btn">X</button>
+                                <button onClick={() => handleDelete(_id)} className="btn bg-orange-500">X</button>
                             </div>
                         </div>
                     </div>
